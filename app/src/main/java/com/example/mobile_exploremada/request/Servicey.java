c115602ec0a563cd1e19aff21528cf3db2357f05
sedra <sedrarandriamananjara23@gmail.com>
2023-08-05 17:54:55 +0300

    fix:register

--- app/src/main/java/com/example/mobile_exploremada/request/Servicey.java
+++ app/src/main/java/com/example/mobile_exploremada/request/Servicey.java
@@ -1,5 +1,6 @@
 package com.example.mobile_exploremada.request;
 
+
 import android.content.Context;
 
 import androidx.fragment.app.FragmentManager;
@@ -8,6 +9,9 @@ import com.example.mobile_exploremada.interceptor.AuthInterceptor;
 import com.example.mobile_exploremada.utils.Credentials;
 import com.example.mobile_exploremada.utils.LieuApi;
 import com.example.mobile_exploremada.utils.LoginService;
+
+import com.example.mobile_exploremada.utils.*;
+
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -32,11 +36,16 @@ public class Servicey {
         return retrofit.create(LoginService.class);
     }
 
+
     public static void addAuthInterceptor(Context context, String authToken, FragmentManager fragmentManager) {
         AuthInterceptor interceptor = new AuthInterceptor(context, authToken, fragmentManager);
         httpClientBuilder.interceptors().clear();
         httpClientBuilder.addInterceptor(interceptor);
         retrofitBuilder.client(httpClientBuilder.build());
         retrofit = retrofitBuilder.build();
+
+    public static RegisterService getRegisterService(){
+        return retrofit.create(RegisterService.class);
+
     }
 }
